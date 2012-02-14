7a407b647c36ddfecdde7aea3d8c9e8dbd6f240a
Waard <Waard@localhost>
2012-02-14 17:27:07 +0100

    ci

--- src/readmp3.py
+++ src/readmp3.py
@@ -1,7 +1,9 @@
-class readmp3:
-""" 
+import eyeD3
+
+class readmp3 (fileName):
+    """ 
     Class that can read an mp3 file's tags using ffmpeg (for now let's focus 
     on artist, album, track number and track name)
-"""
+    """
     
 
---
+++ src/song.py
@@ -0,0 +1,8 @@
+class song:
+    artist = ""
+    album = ""
+    title = ""
+    def __init__(self, artist, album, title):
+        
+        
+    
