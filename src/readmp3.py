bb0f0fa4a6354c02fec1675cba0aaef40f623fa7
Waard <Waard@localhost>
2012-02-16 01:39:54 +0100

    b la

--- src/readmp3.py
+++ src/readmp3.py
@@ -1,9 +1,22 @@
 import eyeD3
+from song import song
 
-class readmp3 (fileName):
+class scan:
     """ 
     Class that can read an mp3 file's tags using ffmpeg (for now let's focus 
     on artist, album, track number and track name)
     """
+    def readmp3(self, fileName):
+        """
+        read some mp3file
+        """
+        tag = eyeD3.Tag()
+        tag.link(fileName)
+        return song(tag.getArtist(), tag.getAlbum(), tag.getTitle())
+
+
+a = scan()
+mp3 = a.readmp3("/media/VIRUS/music/Supertramp/Supertramp - Very Best/01 School.mp3")
+print mp3.title
     
 
