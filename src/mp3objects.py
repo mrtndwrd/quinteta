d5d63192e6de35843e1db08c56ac05d27d43196d
Waard <Waard@localhost>
2012-02-22 16:33:10 +0100

    almost done with hierarchy

---
+++ src/mp3objects.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+class song: 
+    """
+    Object containing the important information of an mp3 file
+    """
+    properties = defaultdict()
+    fileName = ""
+    def __init__(self, fileNameIn, artist="", album="", title=""):
+        self.fileName = fileNameIn
+        self.properties = defaultdict()
+        self.properties['artist'] = artist
+        self.properties['album'] = album
+        self.properties['title'] = title
+
+    def setKey(self, key, value):
+        self.properties[key] = value
+
+class album:
+    """
+    Object containing the import information of an album
+    """
+    properties = defaultdict()
+    albumName = ""
+    songs = []
+    def __init__(self, albumName, artist="Unknown"):
+        self.properties = defaultdict()
+        self.albumName = albumName
+        songs = []
+
+    def addSong(self, song):
+        self.songs.append(song)
+
+class artist:
+    """
+    Object containing the import information of an album
+    """
+    artistName = ""
+    properties = defaultdict()
+    albums = defaultdict()
+    lastAddedAlbum = ""
+    def __init__(self, artistName):
+        self.artistName = artistName
+        self.properties = defaultdict()
+        self.albums = defaultdict(lambda: album(self.lastAddedAlbum, self.artistName)
+    def setArtist(self, artistName):
+        self.artistName = artistName
+    def addSong(self, album, song):
+        self.lastAddedAlbum = albumName
+        self.albums[albumName].addSong(song)
+
--- src/readmp3.py
+++ src/readmp3.py
@@ -1,22 +1,55 @@
 import eyeD3
-from song import song
+from mutagen.easyid3 import EasyID3
+import os
+import copy
+from mp3objects import *
 
 class scan:
     """ 
     Class that can read an mp3 file's tags using ffmpeg (for now let's focus 
     on artist, album, track number and track name)
     """
+    songs = []
+    currentArtistName = ""
+    currentAlbumName = ""
+    albums = defaultdict()
+    artists = defaultdict()
+    def __init__():
+        albums = defaultDict(lambda: album(self.currentAlbumName, self.currentArtistName))
+        artists = defaultdict(lambda: artist(self.currentArtistName))
+    def readall(self, directory):
+        """
+        Read all the mp3 files in a directory and its subdirectory
+        """
+        for direc in os.walk(directory):
+            for file in direc[2]:
+                # check if the file has .mp3 file extension
+                if file.endswith(".mp3"):
+                    try:
+                        songEle = self.readmp3(direc[0] + "/" + file)
+                        self.currentAlbumName = songEle.properties['album'] 
+                        self.currentArtistName = songEle.properties['artist']
+                        self.songs.append(copy.copy(songEle))
+                        self.albums.append(copy.copy(album(songEle.albumName)))
+                    except ValueError:
+                        print "Could not parse " + direc[0] + "/" + file 
+                        print "only saving fileName"
+        return self.songs
     def readmp3(self, fileName):
         """
-        read some mp3file
+        Read some mp3file with file name fileName
         """
-        tag = eyeD3.Tag()
-        tag.link(fileName)
-        return song(tag.getArtist(), tag.getAlbum(), tag.getTitle())
-
+        tag = EasyID3(fileName)
+        keys = ['artist', 'album', 'title']
+        songElement = song(fileName)
+        for key in keys:
+            try:
+                songElement.setKey(key, tag[key])
+            except KeyError:
+                songElement.setKey(key, "Unknown")
+        return songElement
 
 a = scan()
-mp3 = a.readmp3("/media/VIRUS/music/Supertramp/Supertramp - Very Best/01 School.mp3")
-print mp3.title
-    
-
+songs = a.readall("/home/maarten/Music/")
+for s in songs:
+   print s.properties['title']
